1aa99641763563b31097841117a7ea6e9ded70b6
Shawn Huang <shawn.huang.y.h@gmail.com>
2019-05-12 22:13:37 +0800

    update

--- scratch.py
+++ scratch.py
@@ -18,16 +18,13 @@ def getPoi(location , radius , types ,page):
         r1 = requests.get(req_url)
         r1.raise_for_status()
         r1.encoding=r1.apparent_encoding
-        print("服务器连接成功" + pages)                           # 请注意，此处数据抓取成功仅表示能否连接，并不代表数据是否已经获取到。
+        print("服务器连接成功")                           # 请注意，此处数据抓取成功仅表示能否连接，并不代表数据是否已经获取到。
         print(r1.text)                               # 仅供试用，实际情况这里不显示所获取到的数据。
     except:
         print("服务器连接失败，请检查网络连接。")
     return
 
-# 数据导出
-
-
-# 程序循环（暴力取值，取到100页）
+# 程序循环（暴力取值，取到100页）             一个想法：使用BeautifulSoup获取其含有多少项，用项目数去除以20，向上取整，其页数就是应该选择的上限，可以有效地节省时间。（但是能力有限...)
 pages = 1
 while pages < 100:
     getPoi('121.459126,31.162659' , '3000' , '120300' , pages)
