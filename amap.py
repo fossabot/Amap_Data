889f754837383e011afaa54aa86d837577044085
Shawn Huang <shawn.huang.y.h@gmail.com>
2019-05-14 23:25:53 +0800

    Code Update
    Some unnecessary code has been deleted in this commit.

--- amap.py
+++ amap.py
@@ -1,14 +1,9 @@
 import requests
 import json
-from bs4 import BeautifulSoup
 
 # 设置参数key和url
 amap_api_key = ''  # 输入自己的key
 poi_search_url = 'https://restapi.amap.com/v3/place/around?'
-
-# 设置POI爬取区域
-# location = '121.459126,31.162659'
-# radius = '2000'
 
 # 获取单页POI
 def getPoi(location, radius, types, page, times):
@@ -16,18 +11,12 @@ def getPoi(location, radius, types, page, times):
     try:
         result = requests.get(req_url)
         result.raise_for_status()
-#        print(result.text)                               # 仅供调试
         content = result.text
         json_dict = json.loads(content)
         print(json_dict['pois'][k]['name'] +" ," + json_dict['pois'][k]['location'] +" ," + json_dict['pois'][k]['type'])
     except:
         print("获取数据失败。")
     return
-
-# 测试用
-#getPoi('121.459126,31.162659', 2000, 120300, 1, k)
-
-
 
 # 程序循环（暴力取值，计数用i），项数用k。
 i = 1
